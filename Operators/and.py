773d0670472600ca7cbda157b5bd5042a7b55ed3
Jahana Sherin tk <jahanasherintk3@gmail.com>
2023-01-06 09:19:29 +0530

    second

---
+++ Operators/and.py
@@ -0,0 +1,8 @@
+print(0 and 0)
+print(0 and "Rajini")
+print(0 and 100)
+print(0 and -100)
+print(100 and 1000)  #last number should always print if there is 0
+print(10 and 0)
+print(10 and "Rajni")
+print(10 and 100)
