880c7948e9321ff76aed5fc0a9b66a2a198ff1de
Jahana Sherin tk <jahanasherintk3@gmail.com>
2023-01-10 13:37:48 +0530

    Break,Continue

---
+++ JumpStatement/Break.py
@@ -0,0 +1,4 @@
+for x in range(10):
+    if x==5:
+        break
+    print(x)  #it will break at 4 and never print then
---
+++ JumpStatement/continue4.py
@@ -0,0 +1,9 @@
+i=0
+while i<=9:
+    if i!=6:
+        print(i)
+    else:
+        continue
+    i=i+1
+
+    
